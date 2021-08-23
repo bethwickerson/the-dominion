a36c80f04bd306ceab8af3a1ce6c153ab00c1d90
Beth Wickerson <beth.wickerson@signaltheory.com>
2021-08-23 00:21:39 -0500

    take out feed

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,47 +1,38 @@
-import React, { useState, useEffect } from 'react'
+import React/*, { useState, useEffect }*/ from 'react'
 //import youtube from '../apis/youtube';
-import axios from 'axios';
-const API_KEY = 'AIzaSyAKgE715t20jS7WQPXCbaTHiNJ4TZBQVEU';
+// import axios from 'axios';
+// const API_KEY = 'AIzaSyAKgE715t20jS7WQPXCbaTHiNJ4TZBQVEU';
 
 
 export default function Videos() {
 
-  const [videoFeed, setVideoFeed] = useState([]);
+  // const [videoFeed, setVideoFeed] = useState([]);
 
-  const fetchData = async () => {
-    const response = await axios.get('/playlist', {
-      params: {
-        list: 'PLkDsyQofeFSF7Xa8B7Hi5i0IY19HCjCYQ',
-        part: 'snippet',
-        maxResults: 99,
-        key: API_KEY
-      }
-    })
-    setVideoFeed(response.data.items);
-
-  };
-  // Define the function that fetches the data from API
-  // const [fetchData, dispatch] = async () => {
-  //   const { data } = await axios.get('https://www.googleapis.com/youtube/v3/', {
+  // const fetchData = async () => {
+  //   const response = await axios.get('/playlist', {
   //     params: {
+  //       list: 'PLkDsyQofeFSF7Xa8B7Hi5i0IY19HCjCYQ',
   //       part: 'snippet',
   //       maxResults: 99,
   //       key: API_KEY
   //     }
-  //   });
-  //   setVideoFeed(data);
-  //   console.log(videoFeed)
+  //   })
+  //   setVideoFeed(response.data.items);
+
   // };
 
-  // Trigger the fetchData after the initial render by using the useEffect hook
-  useEffect(() => {
-    fetchData();
-  }, []);
+
+  // // Trigger the fetchData after the initial render by using the useEffect hook
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
     <div>
       <h4>Videos</h4>
-
+      {/* {videoFeed.map(post => (
+        <>{post.snippet.title}</>
+      ))} */}
     </div>
   );
 }
