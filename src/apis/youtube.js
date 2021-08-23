e8576106f8d1685a01b5b2b6e343ed50165a5c10
Beth Wickerson <beth.wickerson@signaltheory.com>
2021-08-23 00:12:38 -0500

    youtube api (errors)

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-const KEY = 'AIzaSyAFUNYmE1gfydRFrlb3Q05gXlPSgQmiY6I';
+const KEY = 'AIzaSyAKgE715t20jS7WQPXCbaTHiNJ4TZBQVEU';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
     part: 'snippet',
-    maxResults: 5,
+    maxResults: 99,
     key: KEY
   }
 })
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,32 +1,49 @@
-import React/*, { useState, useEffect }*/ from 'react'
+import React, { useState, useEffect } from 'react'
 //import youtube from '../apis/youtube';
+import axios from 'axios';
+const API_KEY = 'AIzaSyAKgE715t20jS7WQPXCbaTHiNJ4TZBQVEU';
 
 
 export default function Videos() {
 
-  // const [videos, setVideos] = useState([]);
-  // const [selected, setSelected] = null;
-  // handleVideoSelect = (video) => {
-  //   this.setState({ selectedVideo: video })
-  // }
+  const [videoFeed, setVideoFeed] = useState([]);
 
+  const fetchData = async () => {
+    const response = await axios.get('/playlist', {
+      params: {
+        list: 'PLkDsyQofeFSF7Xa8B7Hi5i0IY19HCjCYQ',
+        part: 'snippet',
+        maxResults: 99,
+        key: API_KEY
+      }
+    })
+    setVideoFeed(response.data.items);
+
+  };
   // Define the function that fetches the data from API
-  // const fetchData = async () => {
-  //   const { data } = await axios.get(API_URL);
-  //   setVideos(data);
-
-  //   const imageOptions = {
-  //     scale: 3,
-  //     encoderOptions: 1,
-  //     backgroundColor: 'white',
-  //   }
-  //   svgAsPngUri.svgAsPngUri(document.getElementById("logoTMA"), imageOptions).then(uri =>
-  //     setSelected(uri)
-  //   );
+  // const [fetchData, dispatch] = async () => {
+  //   const { data } = await axios.get('https://www.googleapis.com/youtube/v3/', {
+  //     params: {
+  //       part: 'snippet',
+  //       maxResults: 99,
+  //       key: API_KEY
+  //     }
+  //   });
+  //   setVideoFeed(data);
+  //   console.log(videoFeed)
   // };
+
+  // Trigger the fetchData after the initial render by using the useEffect hook
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h4>Videos</h4>
+      {videoFeed.map(post => (
+        <>{post.snippet.title}</>
+      ))}
     </div>
   );
 }
