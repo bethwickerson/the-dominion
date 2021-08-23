bca167316e5198024073f30c3801ad0d70ca4c8e
Beth Wickerson <beth.wickerson@signaltheory.com>
2021-08-23 09:52:01 -0500

    renders playsinline

--- src/components/Renders.js
+++ src/components/Renders.js
@@ -5,7 +5,7 @@ import videoSource from './renders/video-1628811402.mp4'
 const Renders = () => {
   return (
     <div className="Renders">
-      <video autoPlay="autoplay" muted>
+      <video autoPlay="autoplay" muted playsInline>
         <source src={videoSource} type="video/mp4" />
       </video>
     </div>
